67daba8ef2fdfc5076ca82676f78ece19e28a477
fujisawatk <fuji19880707@gmail.com>
2019-07-13 14:11:28 +0900

    add controller action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+
+
+  private
+  def message_params
+    params.permit(:text, :image)
   end
 end
