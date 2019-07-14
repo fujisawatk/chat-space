2d0fad432ff5589b920cbb21ff6863adbab57629
fujisawatk <fuji19880707@gmail.com>
2019-07-14 18:55:43 +0900

    add FactoryBot file

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryBot.dafine do
+
+  factory :message do
+    text    {Faker::Lorem.sentence}
+    image   {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    user
+    group
+  end
+
+end
