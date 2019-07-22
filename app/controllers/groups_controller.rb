44c7c29197ad37ea586be4def184fc3191d38567
fujisawatk <fuji19880707@gmail.com>
2019-07-22 20:51:51 +0900

    Fixed group editing function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,7 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
+    @users = []
   end
 
   def create
@@ -19,6 +19,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @users = @group.users.where.not(id: current_user.id)
   end
 
   def update
